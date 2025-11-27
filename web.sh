4d212aa07f04267cf171aa0652aa750f1a91de5c
Matt Dailis <dwehttam@gmail.com>
2025-11-26 23:39:36 -0800

    Change invocation of worker.py

--- web.sh
+++ web.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-python -c 'from neira_flask import worker; worker.listen_for_jobs()' &
+#python -c 'from neira_flask import worker; worker.listen_for_jobs()' &
+python neira_flask/worker.py &
 
 gunicorn --config gunicorn.conf.py neira_flask.main:app
 
